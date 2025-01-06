36926f837481ec8e2f98225ac926228066d663d7
PUMBA32 <sokolovgames42@gmail.com>
2025-01-06 09:15:24 +0300

    Float class was created

---
+++ float/float.py
@@ -0,0 +1,7 @@
+class Float: 
+    def __init__(self, value: float) -> None:
+        self.value = value
+
+
+    def __str__(self) -> str:
+        return f'<class: FLoat, value: {self.value}>'
---
+++ float/test.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestFloat(unittest.TestCase): ...
+
+
+if __name__ == '__main__':
+    unittest.main()
